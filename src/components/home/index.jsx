4c2d59ef9e5a86ef7f06174cf81a22c6d630bbf1
RobinRz <robinkurian1997@gmail.com>
2023-09-23 19:27:51 +0530

    update welcome page

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,13 +1,11 @@
-import DefaultLayout from '../layouts/DefaultLayout';
+import { useState } from "react";
 
-const Home = () => {
-
+const Home = ({ user, todos }) => {
+  
   return (
-    <DefaultLayout showFooter={true}>
-      <div className='w-full h-[calc(100vh-100px)]  flex justify-center items-center overflow-y-auto overflow-x-hidden '>
-        <span className='text-heading-1'>Home Page</span>
-      </div>
-    </DefaultLayout>
+    <div className='w-full h-[calc(100vh-100px)] flex justify-center items-center overflow-y-auto overflow-x-hidden '>
+      <div className='text-heading-1'>Welcome {user}</div>
+    </div>
   );
 };
 
