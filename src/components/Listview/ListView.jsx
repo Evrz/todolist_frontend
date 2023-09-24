9753ab824b1b72ccaf609a18a570605260e143d8
sreeragmsurendran <sreeragmsurendran1997@gmail.com>
2023-09-24 21:21:20 +0530

    feature : rebase with QA

--- src/components/Listview/ListView.jsx
+++ src/components/Listview/ListView.jsx
@@ -2,25 +2,25 @@ import React, { useEffect, useState } from 'react'
 import ListViewItem from '../listviewitem/ListViewItem';
 import axios from 'axios';
 
- const ListView = ({ className }) => {
+const ListView = ({ className }) => {
 
-  const [data, setData] = useState([]);
+    const [data, setData] = useState([]);
 
-  useEffect(() => {
-      axios.get('/api/v1/todo/items')
-          .then(response => {
-              setData(response.data);
-          })
-          .catch(error => {
-              console.error(error);
-          });
-  }, [])
+    useEffect(() => {
+        axios.get('/api/v1/todo/items')
+            .then(response => {
+                setData(response.data);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }, [])
 
     return (
         <div className={`h-4/5 overflow-auto ${className}`}>
             {data.map((item) => (
                 <div className='flex justify-between'>
-                   <ListViewItem item= {item}/>
+                    <ListViewItem item={item} />
                 </div>
             ))}</div>
     )
