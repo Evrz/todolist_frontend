c20faf4f2fa7de510408a0adce4125a93d50d3bc
RobinRz <robinkurian1997@gmail.com>
2023-09-24 21:07:18 +0530

    style fix:title in title bar

--- src/components/common/TitleBar.jsx
+++ src/components/common/TitleBar.jsx
@@ -2,7 +2,7 @@
 const TitleBar = ({ title, children }) => {
   return (
     <div className='flex px-7 py-3 text-heading-4 justify-between'>
-      <div className=''>{title}</div>
+      <div className='flex items-center'>{title}</div>
       <div>{children}</div>
     </div>
   )
