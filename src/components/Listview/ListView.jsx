5eb85f87c7a1de91e3fe3661bad99ef0cd370c91
RobinRz <robinkurian1997@gmail.com>
2023-09-24 20:04:57 +0530

    add sidebar, refactors on Listview styles

--- src/components/Listview/ListView.jsx
+++ src/components/Listview/ListView.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import ListViewItem from '../Listviewitem/ListViewItem';
 
- const ListView = () => {
+const ListView = ({ className }) => {
     //mimic the API data with dummy data
-    let ListItems =[{item:"jkshjkhdhksd"},{item:"jkshjkhdhksd"},{item:"jkshjkhdhksd"},{item:"jkshjkhdhksd"},{item:"jkshjkhdhksd"},{item:"jkshjkhdhksd"},{item:"jkshjkhdhksd"},{item:"jkshjkhdhksd"},{item:"jkshjkhdhksd"},{item:"jkshjkhdhksd"},{item:"jkshjkhdhksd"},{item:"jkshjkhdhksd"}]
-    return (
+    let listItems = [
+        { item: "jkshjkhdhksd" }, { item: "jkshjkhdhksd" }, { item: "jkshjkhdhksd" }, { item: "jkshjkhdhksd" }, { item: "jkshjkhdhksd" }, { item: "jkshjkhdhksd" }, { item: "jkshjkhdhksd" }, { item: "jkshjkhdhksd" }, { item: "jkshjkhdhksd" }, { item: "jkshjkhdhksd" }, { item: "jkshjkhdhksd" }, { item: "jkshjkhdhksd" }]
 
-        <div className='h-80 overflow-auto'>
-            {ListItems.map((item) => (
+    // TODO: REMOVE listItems ONCE API DATA IS LOADED
+
+    return (
+        <div className={`h-4/5 overflow-auto ${className}`}>
+            {listItems.map((item) => (
                 <div className='flex justify-between'>
-                   <ListViewItem item= {item}/>
+                    <ListViewItem item={item} />
                 </div>
             ))}</div>
     )
--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -3,9 +3,18 @@ import ListView from '../ListView/ListView'
 const Home = () => {
 
   return (
-    <DefaultLayout showFooter={true}>
-      <div className='bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 bg-opacity-30 p-4'>
-        <ListView />
+    <DefaultLayout>
+      <div className='flex h-full test justify-evenly'>
+        <div className='w-[15%] flex justify-around m-4 '>
+          <div className='text-heading-8 w-[75%] p-5 bg-slate-500 rounded-md'>
+            {/* TODO: REPLACE WITH SIDEBAR COMPONENT */}
+            sidebar
+          </div>
+        </div>
+        <div className='w-[85%] bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 bg-opacity-30 m-4 rounded-md'>
+          <div className='px-7 py-3 text-heading-4'> Todos</div>
+          <ListView className={'px-4'} />
+        </div>
       </div>
     </DefaultLayout>
   );
