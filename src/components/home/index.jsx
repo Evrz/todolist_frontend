89a875218ff564cb321d5d1057391bdac0af3c7d
RobinRz <robinkurian1997@gmail.com>
2023-09-24 19:05:50 +0530

    ListView import fix, refactor

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,11 +1,11 @@
 import DefaultLayout from '../layouts/DefaultLayout';
-
+import ListView from '../ListView/ListView'
 const Home = () => {
 
   return (
     <DefaultLayout showFooter={true}>
       <div className='bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 bg-opacity-30 p-4'>
-        <ListView/>
+        <ListView />
       </div>
     </DefaultLayout>
   );
