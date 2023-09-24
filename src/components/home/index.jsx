91b7ffd831426f3002d67f8d636162cd37d22a22
RobinRz <robinkurian1997@gmail.com>
2023-09-24 20:31:42 +0530

    convert SideBar to component

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,3 +1,4 @@
+import SideBar from '../common/SideBar';
 import DefaultLayout from '../layouts/DefaultLayout';
 import ListView from '../ListView/ListView'
 const Home = () => {
@@ -5,13 +6,10 @@ const Home = () => {
   return (
     <DefaultLayout>
       <div className='flex h-full test justify-evenly'>
-        <div className='w-[15%] flex justify-around m-4 '>
-          <div className='text-heading-8 w-[75%] p-5 bg-slate-500 rounded-md'>
-            {/* TODO: REPLACE WITH SIDEBAR COMPONENT */}
-            sidebar
-          </div>
+        <div className='w-[10%] flex justify-around m-4 '>
+          <SideBar />
         </div>
-        <div className='w-[85%] bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 bg-opacity-30 m-4 rounded-md'>
+        <div className='w-[90%] bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 bg-opacity-30 m-4 rounded-md'>
           <div className='px-7 py-3 text-heading-4'> Todos</div>
           <ListView className={'px-4'} />
         </div>
