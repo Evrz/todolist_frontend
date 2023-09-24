3c13bb76abe21242624cc7180c7daf680fcdf5fc
RobinRz <robinkurian1997@gmail.com>
2023-09-24 22:14:33 +0530

    dynamic sidebar items, with data for Button, icon

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -9,14 +9,13 @@ const Home = () => {
     {
       id: 1,
       label: "Todo",
-      src: "assets/icons/todo",
+      icon: {
+        src: "/assets/icons/todoIcon.png",
+        width: "10px",
+        height: "10px",
+        alt: "todo icon"
+      },
       onClick: () => { console.log('clicked todo') }
-    },
-    {
-      id: 1,
-      label: "Project",
-      src: "assets/icons/project",
-      onClick: () => { console.log('clicked projects') }
     }
   ]
 
@@ -26,12 +25,19 @@ const Home = () => {
 
   return (
     <DefaultLayout>
-      <div className='flex h-full test justify-evenly'>
+      <div className='flex h-full justify-evenly'>
         <div className='w-[10%] flex justify-around m-4 '>
-          <SideBar >
+          <SideBar className={'pt-6'} >
             {sideBarItems.map((item) => (
               <div className='cursor-pointer'
-                onClick={item.onClick}>{item.label}
+                onClick={item.onClick}>
+                <Button
+                  buttonType={'icon'}
+                  data={item}
+                  onClick={handleAddTodo}
+                  variant="primary"
+                  className={'mr-2'}
+                />
               </div>))
             }
           </SideBar>
@@ -39,6 +45,7 @@ const Home = () => {
         <div className='w-[90%] bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 bg-opacity-30 m-4 rounded-md'>
           <TitleBar title={'Your Todos'}>
             <Button
+              buttonType={'button'}
               onClick={handleAddTodo}
               text="Add ToDo"
               variant="primary"
