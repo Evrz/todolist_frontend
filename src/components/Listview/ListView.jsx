70494e023712c9ee937975b8621aac6b01f43ee1
sreeragmsurendran <sreeragmsurendran1997@gmail.com>
2023-09-24 20:48:01 +0530

    Feature:Api INtegration changes

--- src/components/Listview/ListView.jsx
+++ src/components/Listview/ListView.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ListViewItem from '../Listviewitem/ListViewItem';
+import axios from 'axios';
+
 
  const ListView = () => {
+
+
+    
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+      axios.get('/api/v1/todo/items')
+          .then(response => {
+              setData(response.data);
+          })
+          .catch(error => {
+              console.error(error);
+          });
+  }, []);
+console.log(data,"asdjadsadljea")
     //mimic the API data with dummy data
-    let ListItems =[{item:"jkshjkhdhksd"},{item:"jkshjkhdhksd"},{item:"jkshjkhdhksd"},{item:"jkshjkhdhksd"},{item:"jkshjkhdhksd"},{item:"jkshjkhdhksd"},{item:"jkshjkhdhksd"},{item:"jkshjkhdhksd"},{item:"jkshjkhdhksd"},{item:"jkshjkhdhksd"},{item:"jkshjkhdhksd"},{item:"jkshjkhdhksd"}]
+    let ListItems =data
     return (
 
         <div className='h-80 overflow-auto'>
