9ea317ffb58b134db1cd5cece9642622ced3b8a2
RobinRz <robinkurian1997@gmail.com>
2023-09-24 19:26:11 +0530

    refactor header and footer

--- src/components/layouts/DefaultFooter.jsx
+++ src/components/layouts/DefaultFooter.jsx
@@ -4,7 +4,7 @@ const DefaultFooter = () => {
   return (
     <footer className='w-full bg-red-300 px-5 py-3 shadow-[0_2px_10px_0px_rgba(0,0,0,0.4)]'>
       <ContentWrapper>
-        <div className='text-xs text-[#888]'> Evrz &#x2022; Starter Project &#x2022; Reactjs Tailwind </div>
+        <div className='text-xs text-[#424242]'> Evrz &#x2022; Todo List Project &#x2022; Reactjs </div>
       </ContentWrapper>
     </footer>
   );
--- src/components/layouts/DefaultHeader.jsx
+++ src/components/layouts/DefaultHeader.jsx
@@ -5,13 +5,10 @@ const DefaultHeader = () => {
   return (
     <div className={'w-full h-16 bg-color-title-text border-b-[1.5px]'}>
       <ContentWrapper>
-        <div className='w-full h-full py-1 px-10 pl-5'>
-          <h1 className="text-heading-3 leading-10 text-color-secondary-s80">
-            Starter Project: React
+        <div className='w-full py-1 px-10 pl-5 items-center'>
+          <h1 className="text-heading-3 text-color-secondary-s80">
+            TODO LIST: React
           </h1>
-          <p className="text-color-secondary-s70 text-sm mb-4">
-            Welcome to our React Project
-          </p>
         </div>
       </ContentWrapper>
     </div>
